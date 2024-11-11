526e3c7c6732f9d636b3f44ff841670eb078ed31
chatelao <olivier.chatelain+xduino@gmail.com>
2024-11-11 21:20:35 +0100

    Add - DCC/RailCom data from RTC

---
+++ testdata/rtbdcc_trace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/chatelao/ws/wireshark/build/run/text2pcap  -E user13 -t '%S.%f'  -D -r '^(?<dir>[IO])\s(?<time>\d\d.\d+)\s(?<data>[0-9a-fA-F]+)$' rtbdcc_trace.hex rtbdcc_trace.pcap
